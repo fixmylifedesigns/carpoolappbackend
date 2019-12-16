1f316135af75b4b23750a38dc1a6b46a634d1ee5
fixmylifedesigns <ijd.irving@gmail.com>
2019-12-16 18:29:32 -0500

    get user by id

--- router/auth/middleware/restrictedMiddleware.js
+++ router/auth/middleware/restrictedMiddleware.js
@@ -12,19 +12,4 @@ module.exports = (req, res, next) => {
               next()
           }
       })
-  // }
-//   const token = req.body.token ? req.body.token : req.headers.authorization;
-//   if (token) {
-//     jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
-//       if (err) {
-//         res.status(401).json({ message: "Invalid Credentials" });
-//       } else {
-//         req.decodedToken = decodedToken;
-//         delete req.body.token;
-//         next();
-//       }
-//     });
-//   } else {
-//     res.status(401).json({ message: "No token provided!" });
-//   }
 };
--- router/users/usersRouter.js
+++ router/users/usersRouter.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
+const restricted = require("../auth/middleware/restrictedMiddleware");
 
 const Users = require("./usersModel");
 
-router.get("/", (req, res) => {
+router.get("/all", (req, res) => {
   Users.getUsers()
     .then(users => {
       res.status(200).json(users);
@@ -14,16 +15,28 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/", restricted, (req, res) => {
+  Users.findById(req.decodedToken.id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ err, message: "we ran into an error retreving the user" });
+    });
+});
+
 router.get("/:id", (req, res) => {
-    Users.findById(req.params.id)
-      .then(user => {
-        res.status(200).json(user);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ err, message: "we ran into an error retreving the user" });
-      });
-  });
+  Users.findById(req.params.id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ err, message: "we ran into an error retreving the user" });
+    });
+});
 
 module.exports = router;
