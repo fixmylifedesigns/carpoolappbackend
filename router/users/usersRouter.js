12daee5010d1154cb74784546606a9c3402b1d2b
fixmylifedesigns <ijd.irving@gmail.com>
2019-12-17 14:04:33 -0500

    cleaning code

--- router/users/usersRouter.js
+++ router/users/usersRouter.js
@@ -5,20 +5,7 @@ const Booked = require("../bookRide/bookRidesModel");
 const Users = require("./usersModel");
 const Rides = require("../postRide/postsRidesModel");
 
-router.get("/all", (req, res) => {
-  Users.getUsers()
-    .then(users => {
-      res.status(200).json(users);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ err, message: "Cannot retrieve users from database." });
-    });
-});
-
-router.get("/", restricted, (req, res) => {
-  const user_id = req.decodedToken.id;
+const UserInfo = (user_id, res, req) => {
   Users.findById(user_id)
     .then(user => {
       Booked.findByPassanger(user_id).then(booked => {
@@ -33,7 +20,8 @@ router.get("/", restricted, (req, res) => {
           user.posted_rides = [];
         } else {
           user.posted_rides = rides;
-        } return res.status(200).json(user);
+        }
+        return res.status(200).json(user);
       });
     })
     .catch(err => {
@@ -41,33 +29,28 @@ router.get("/", restricted, (req, res) => {
         .status(500)
         .json({ err, message: "we ran into an error retreving the user" });
     });
+};
+
+router.get("/all", (req, res) => {
+  Users.getUsers()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ err, message: "Cannot retrieve users from database." });
+    });
+});
+
+router.get("/", restricted, (req, res) => {
+  const user_id = req.decodedToken.id;
+  UserInfo(user_id, res, req);
 });
 
 router.get("/:id", (req, res) => {
   const user_id = req.params.id;
-  Users.findById(req.params.id)
-    .then(user => {
-      Booked.findByPassanger(user_id).then(booked => {
-        if (!booked) {
-          user.booked_rides = [];
-        } else {
-          user.booked_rides = booked;
-        }
-        Rides.getUserPosts(user_id).then(rides => {
-          if (!rides) {
-            user.posted_rides = [];
-          } else {
-            user.posted_rides = rides;
-          }
-          return res.status(200).json(user);
-        });
-      });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ err, message: "we ran into an error retreving the user" });
-    });
+  UserInfo(user_id, res, req);
 });
 
 module.exports = router;
